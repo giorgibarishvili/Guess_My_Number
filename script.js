b893f17ac2bdb201b86f6bd7e665860c98265823
Giorgi Barishvili <barishvili2001@gmail.com>
2023-04-15 00:58:32 +0400

    project done

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct number: '
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 23
-
-const secretNumber = Math.trunc(Math.random()*20)+1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
 
 document.querySelector('.check')
 .addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
-    if (!guess) {
-        document.querySelector('.message').textContent = 'Enter number!';
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct number! 👌';
-    } else if (guess > secretNumber) {
-        if (score > 0) {
-            document.querySelector('.message').textContent = 'Number is too high';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
+    if (!guess) { //როდესაც რიცხვი არაა შეყვანილი
+        displayMessage('Enter number!');
+    } else if (guess === secretNumber) { //როდესაც თამაში მოიგო
+        displayMessage('Correct number! 👌');
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width= '30rem';
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
         }
-    } else if (guess < secretNumber) {
+    } else if (guess !== secretNumber) { //რიცხვი არ დაემთხვა
         if (score > 0) {
-            document.querySelector('.message').textContent = 'Number is too low';
+            displayMessage(guess > secretNumber ? 'Number is too high' : 'Number is too low');
             score--;
             document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
+        } else { // თამაში წაგებულია
+            displayMessage('You lost the game!');
         }
     }
+})
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width= '15rem';
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
 })
